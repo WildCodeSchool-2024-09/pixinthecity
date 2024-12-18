faf4e0a0a5370f7e740253e175acc25cfd9b371c
maldonae <maldonadoeric69120@gmail.com>
2024-12-18 15:28:28 +0100

    Page redaction des règles et mise en place des badges

--- client/src/pages/PagesClassiques/Regles.tsx
+++ client/src/pages/PagesClassiques/Regles.tsx
@@ -1,10 +1,91 @@
 function Regles() {
-    return (
-      <>
-        <h2>Les règles</h2>
-      </>
-    );
-  }
-  
-  export default Regles;
-  
+  return (
+    <>
+      <h2>
+        12. "Prêts à vivre l’aventure ? Découvrez nos règles et rejoignez-nous
+        !" 13. "Des règles simples pour une aventure inoubliable." "Vous
+        parcourez les 14. rues comme un historien moderne, redécouvrant les
+        trésors cachés de nos 15. villes." "Des récompenses pour les
+        contributeurs assidus et un 16. classement officiel." 17.
+      </h2>
+      18. <h3>Système de Points</h3>
+      <ul>
+        <li>
+          Soumission d'une œuvre avec photo et localisation valide : +50 points.
+        </li>
+        <li>
+          Première soumission d'une œuvre dans une nouvelle ville/quartier :
+          +100 points.
+        </li>
+        22. <li>Signalement d'une œuvre disparue (avec preuve) : +30 points</li>
+      </ul>
+      24. <h3>Système de Badges</h3>
+      <p>
+        Les badges sont des récompenses visuelles qui sont attribués aux
+        contributeurs :
+      </p>
+      27. <h4>Par contribution</h4>
+      <ul>
+        <li>
+          Soumission d'une œuvre avec photo et localisation valide : +50 points.
+        </li>
+
+        <li>
+          Découvreur Novice : Soumettre 5 œuvres.
+          <img src="\src\assets\images\novice.jpg" alt="novice" />
+        </li>
+        <li>
+          Photographe de Rue : Soumettre 50 œuvres.
+          <img src="\src\assets\images\photographe.png" alt="photographe" />
+        </li>
+        <li>
+          Archiviste : Soumettre 200 œuvres.
+          <img src="\src\assets\images\archiviste.jpg" alt="archiviste" />
+        </li>
+      </ul>
+      <h4>Par engagement géographique</h4>
+      <ul>
+        <li>
+          Explorateur Local : Soumettre des œuvres dans 3 quartiers d'une même
+          ville.
+          <img
+            src="\src\assets\images\explorateur_local.jpg"
+            alt="explorateur"
+          />
+        </li>
+
+        <li>
+          Globe-Trotter Urbain : Soumettre des œuvres dans 2 villes différentes.
+          <img src="\src\assets\images\globe_trotteur.png" alt="globe" />
+        </li>
+      </ul>
+      <h4>Badges spéciaux</h4>
+      <ul>
+        <li>
+          Découverte Rare : Soumettre une œuvre d’un artiste renommé.
+          <img src="\src\assets\images\decorateur_rare.jpg" alt="decouverte" />
+        </li>
+        <li>
+          Oeil de Lynx : Soumettre une œuvre difficile à localiser.
+          <img src="\src\assets\images\oeil_de_lynx.png" alt="oeil" />
+        </li>
+      </ul>
+      <h3>Niveaux</h3>
+      <p>
+        Les niveaux permettent de suivre la progression globale des
+        contributeurs en fonction du total de points accumulés. Plus vous aurez
+        de points plus vous serez au sommet de la pyramide :
+      </p>
+      <ul>
+        <li>Passant Curieux (0-499 points)</li>
+        21. <li>Apprenti Explorateur (500-999 points)</li>
+        22. <li>Chercheur Urbain (1,000-2,499 points)</li>
+        21. <li>Découvreur Passionné (2,500-4,999 points)</li>
+        22. <li>Conservateur de Rue (5,000-9,999 points)</li>
+        22. <li>Légende du Street Art (10,000+ points)</li>
+      </ul>
+    </>
+  );
+}
+
+export default Regles;
