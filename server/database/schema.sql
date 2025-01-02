0add4f505671c1a8f74200941819363d07fe9d97
maldonae <maldonadoeric69120@gmail.com>
2025-01-02 17:01:38 +0100

    Création database et insertion des trois users

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,11 @@
 create table user (
   id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+  firstname varchar(255) not null,
+  lastname varchar(255) not null
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
-);
-
-insert into user(id, email, password)
+insert into user(firstname, lastname)
 values
-  (1, "jdoe@mail.com", "123456");
-
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+  ("Jacqueline", "Morin"),
+  ("René", "Pichard"),
+  ("Pépito", "Perez");
