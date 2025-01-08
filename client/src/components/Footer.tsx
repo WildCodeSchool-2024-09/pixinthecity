4ef334d851ef627dc149333df58bb5898bb3a236
Faccinetto <bastien.faccinetto@gmail.com>
2025-01-08 10:47:01 +0100

    création du composant footer en format mobile

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,12 +1,48 @@
+import "./CSS/Footer.css";
+
 function Footer() {
   return (
-    <>
-      <p>Carte</p>
-      <p>Règles</p>
-      <img src="http://bouton.fr" alt="IconeProfil" />
-      <p>Classement</p>
-      <p>Contact</p>
-    </>
+    <footer>
+      <button type="button" className="button_footer">
+        <img
+          src={"/src/assets/images/footer_icons/icon_marker.png"}
+          alt="marker"
+          className="icon_footer"
+        />
+        <p>CARTE</p>
+      </button>
+      <button type="button" className="button_footer">
+        <img
+          src={"/src/assets/images/footer_icons/icon_ranking.png"}
+          alt="ranking"
+          className="icon_footer"
+        />
+        <p>CLASSEMENT</p>
+      </button>
+      <button type="button" className="button_camera">
+        <img
+          src={"/src/assets/images/footer_icons/icon_camera.png"}
+          alt="camera"
+          className="icon_camera"
+        />
+      </button>
+      <button type="button" className="button_footer">
+        <img
+          src={"/src/assets/images/footer_icons/icon_rules.png"}
+          alt="rules"
+          className="icon_footer"
+        />
+        <p>RÈGLES</p>
+      </button>
+      <button type="button" className="button_footer">
+        <img
+          src={"/src/assets/images/footer_icons/icon_contact.png"}
+          alt="contact"
+          className="icon_footer"
+        />
+        <p>CONTACT</p>
+      </button>
+    </footer>
   );
 }
 
